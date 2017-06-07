8658851c068ed6eb6e0d5941e2540fe513558d4f
William Derksen <william.derksen@students.olin.edu>
2017-06-07 12:11:16 -0400

    added plane saving

--- scripts/plane_fitter.py
+++ scripts/plane_fitter.py
@@ -2,6 +2,7 @@
 
 import rospy
 from sensor_msgs.msg import PointCloud
+from geometry_msgs.msg import Pose
 import numpy as np
 import pcl
 import tf
@@ -9,6 +10,7 @@ from threading import Lock
 import time
 #import copy
 import tf
+import math
 
 class PlaneFitter(object):
     def __init__(self):
@@ -52,13 +54,16 @@ class PlaneFitter(object):
         self.planetrynum = 5
         self.verticalityThreshold = .4
         self.t = 0.0
-        self.robustThreshold = 100;
+        self.robustThreshold = 100
+        self.ycutoff = 0;
+        self.saved_plane_model = None;
+        self.untransformed_saved_plane_model = None;
         #self.newP = False;
 
     def process_cloud(self, msg):
         self.m.acquire()
         self.actualP = msg
-        self.actualP.points = self.actualP.points[::10]
+        self.actualP.points = [i for i in self.actualP.points[::10] if i.y > self.ycutoff]
         #self.CurrP = np.asarray([(p.x, p.y, p.z) for p in self.actualP.points], dtype = np.float32)
         #print "got a cloud", self.CurrP.shape
         self.m.release()
@@ -85,15 +90,15 @@ class PlaneFitter(object):
                 #Setting Point Cloud
                 self.m.acquire()
                 #set_pose
-                self.CurrP, actualPCopy = self.depth_to_odom(self.actualP)
+                self.CurrP, actualPCopy = self.pcloud_transform(self.actualP, '/odom', True)
 
                 if actualPCopy is None:
                     pass
                 else:
                     #skip = False
                     cloud_filtered = pcl.PointCloud(self.CurrP) #Move To PointCloud2 Eventually!
-                    print "cloudsize: ", str(cloud_filtered.size)
-                    #fil = self.UseP.make_passthrough_filter()
+                    #print "cloudsize: ", str(cloud_filtered.size)
+                    #fil = cloud_filtered.make_passthrough_filter()
                     #fil.set_filter_field_name("z")
                     #fil.set_filter_limits(0, 999)
                     #cloud_filtered = self.UseP#fil.filter()
@@ -109,23 +114,66 @@ class PlaneFitter(object):
                     seg.set_distance_threshold(.03)
                     #seg.set
                     #self.CurrP = cloud_filtered.to_array
+                    match = False
                     try:
                         for i in range(self.planetrynum):
-                            print i
+                            #print i
+                            check = False
+                            usesave = None
                             indices, plane_model = seg.segment()
+                            if self.saved_plane_model is not None :
+                                if (abs(plane_model[0]-self.saved_plane_model[0]) < .075 and
+                                    abs(plane_model[1]-self.saved_plane_model[1]) < .075 and
+                                    abs(plane_model[2]-self.saved_plane_model[2]) < .075 and
+                                    abs(plane_model[3]-self.saved_plane_model[3]) < .2):
+                                    match = True
+                                else:
+                                    check = True
                             if len(indices) < self.robustThreshold:
-                                print len(indices)
-                                print("ran out of robust planes")
+                                #print len(indices)
+                                print("No Robust Planes")
+
+                                print "wall_distance: " + str(abs(self.untransformed_saved_plane_model[3]))
                                 break
-                            if abs(plane_model[2]) < self.verticalityThreshold:
+                            if abs(plane_model[2]) < self.verticalityThreshold or match:
+                                #new_plane_model = None
+
                                 self.cloud_plane = cloud_filtered.extract(indices, negative=False) #Extracting plane points
                                 actualPCopy.points  = [actualPCopy.points[indx] for indx in indices]
-                                self.pub.publish(actualPCopy)
-                                #print "Plane Num " + str(i)
-                                print "plane model: " + str(plane_model)
-                                print "plane_size: " + str(self.cloud_plane.size)
-                                print "wall_distance: " + str(plane_model[3])
-                                #cloud_filtered = cloud_filtered.extract(indices, negative=True)
+
+                                untransformed_CurrP, actualPCopy = self.pcloud_transform(actualPCopy, "/depth_camera", True)
+                                untransformed_plane_cloud = pcl.PointCloud(untransformed_CurrP)
+
+                                seg = untransformed_plane_cloud.make_segmenter()
+                                seg.set_optimize_coefficients(True)
+                                seg.set_model_type(pcl.SACMODEL_PLANE)
+                                seg.set_method_type(pcl.SAC_RANSAC)
+                                seg.set_distance_threshold(.03)
+                                new_indices, new_plane_model = seg.segment()
+
+                                #if match:
+                                #    self.saved_plane_model = plane_model
+                                #    self.untransformed_saved_plane_model = new_plane_model
+                                if check:
+                                    if abs(new_plane_model[3]) > abs(self.untransformed_saved_plane_model[3]):
+                                        usesave = True
+                                    else:
+                                        usesave = False
+                                if usesave:
+                                    print("SAVE USED")
+                                    #print "plane model: " + str(self.untransformed_saved_plane_model)
+                                    #new_plane_model = planetransform(plane_model, "/depth_camera")
+                                    print "wall_distance: " + str(abs(self.untransformed_saved_plane_model[3]))
+                                    break
+
+                                #self.pub.publish(actualPCopy)
+
+                                #print "plane model: " + str(plane_model)
+                                #print "plane_size: " + str(self.cloud_plane.size)
+                                print "wall_distance: " + str(abs(new_plane_model[3]))
+                                self.saved_plane_model = plane_model
+                                self.untransformed_saved_plane_model = new_plane_model
+
                                 break
                             else:
                                 for a in indices:
@@ -141,8 +189,8 @@ class PlaneFitter(object):
                                 continue
                             #self.cloud_plane.to_file("cloud_plane.pcd")
                     except Exception as inst:
-                        print inst, i
-
+                        pass
+                        #print inst, i
                 self.actualP = None
                 self.UseP = None
                 self.m.release()
@@ -152,7 +200,7 @@ class PlaneFitter(object):
             #
             #self.newP = False
             r.sleep()
-    def depth_to_odom(self, cloud):
+    def pcloud_transform(self, cloud, target_frame, get_points):
         """        self.t = self.listener.getLatestCommonTime('/depth_camera', '/odom')
         t_vals, r_quat = self.listener.lookupTransform("/depth_camera", "/odom", self.t)
         transform = self.listener.fromTranslationRotation(t_vals, r_quat)
@@ -164,15 +212,17 @@ class PlaneFitter(object):
             cloud.points[i].y = d4points[i][1]
             cloud.points[i].z = d4points[i][2]
         return points, cloud"""
-
         try:
             if np.mean([p.z for p in cloud.points]) > 10**3:
                 return None, None
-            newcloud = self.listener.transformPointCloud('/odom', cloud)
+            newcloud = self.listener.transformPointCloud(target_frame, cloud)
             if np.isnan(np.mean([p.z for p in newcloud.points])):
                 return None, None
             self.pub_transformed.publish(newcloud)
-            points = np.asarray([(p.x, p.y, p.z) for p in newcloud.points], dtype = np.float32)
+            if get_points is True:
+                points = np.asarray([(p.x, p.y, p.z) for p in newcloud.points], dtype = np.float32)
+            else:
+                points = None
         except Exception as inst:
             print inst
             newcloud = None
@@ -188,6 +238,19 @@ class PlaneFitter(object):
         P_cam = np.hstack([pointlist, ones])
         P_odom = np.matmul(P_cam, r_mat)
         return P_odom[:,:3]"""
+    def planetransform(self, plane, target_frame):
+        t = self.listener.getLatestCommonTime('/odom', '/depth_camera')
+        t_vals = (self.listener.lookupTransform('/odom', '/depth_camera', t))[0]
+        r_quat = (self.listener.lookupTransform('/odom', '/depth_camera', t))[1]
+        r_mat = tf.transformations.quaternion_matrix(r_quat)
+        m = 1
+        perppoint = plane[0:3] * plane[3]
+        useperppoint = np.array(perppoint + [1]).T
+        newperppoint = np.matmul(r_mat, useperppoint)
+        distance = math.sqrt(math.pow(newperppoint[0],2) + math.pow(newperppoint[1],2) + math.pow(newperppoint[2],2))
+        newperppoint = [i/distance for i in newperppoint]
+        return newperppoint + [distance]
+
 
 if __name__ == '__main__':
     node = PlaneFitter()
